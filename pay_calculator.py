890f5210174d77cf76fe12f4394fe5c3c1ebc5a2
hobojoe1848 <sequeira.julian@gmail.com>
2015-11-20 09:05:38 +1100

    Added lots of comments and changed input variable
    Added comments for my own understanding.
    Also, the input variable was set to an int.
    I changed that to a float so it wouldn't need to be converted later
    (removed the conversion line too).
    
    Adjusted a lot of comment formatting to make it easier to read.

--- pay_calculator.py
+++ pay_calculator.py
@@ -3,22 +3,24 @@ import sys
 
 refresh = False
 if len(sys.argv) > 1: 
-  refresh = True # if called with cli arg (any string) do the questioning again
+  refresh = True                      # if script is run with cli argument (any string) do the questioning again
 
-if os.path.isfile("userattributes.txt") and not refresh:        #Check to see userattributes file exists (== True not needed) - if refresh go to else
-    print("Welcome Back!")                              #If it exists, print Welcome Back Message        
+if os.path.isfile("userattributes.txt") and not refresh:      #Check to see userattributes file exists (== True not needed) - if refresh chosen go to else
+    print("Welcome Back!")                                    #If it exists, print Welcome Back Message        
 else:
-    # new file or refresh
-    configfile = open("userattributes.txt", "w")        #If userattributes doesn't exist, create new userattributes file
+
+    # start creating the new userattributes file or reset values if refresh is chosen
+    
+    configfile = open("userattributes.txt", "w")              #If userattributes doesn't exist, create new userattributes file. Open it in variable configfile
 
     while True:
-      try:
-        normaltime = int(input('Please enter normal hourly rate: '))
-        break
+      try:                                                    # try is an error handling function. Try doing this, if an error occurs, go to the exception.
+        normaltime = float(input('Please enter normal hourly rate: '))  #enter normal hourly pay rate
+        break                                                 #break our of while loop
       except ValueError:
-        print("Not a number, please try again")
+        print("Not a number, please try again")         #if anything other than an int is entered, error out.
 
-    normaltime = float(normaltime)                      #Convert user input to float
+    #normaltime = float(normaltime)                      #Convert user input to float
     
     timeandhalf = normaltime * 1.5                      #Calculate time and a half and double time rates based on normal pay rate
     doubletime = normaltime * 2
@@ -33,9 +35,12 @@ else:
     configfile.write(str(timeandhalf) + "\n");
     configfile.write(str(doubletime) + "\n");
     
-    configfile.close()
+    configfile.close()                                  #close off the userattributes file (configfile variable).
 
-configfile = open("userattributes.txt", "r")
+
+#Now that the userattributes have been collected and stored, let's start the actual program
+
+configfile = open("userattributes.txt", "r")            #open the userattributes file in variable "configfile".
 
 
 
