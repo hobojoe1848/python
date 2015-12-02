89f720c30eeb45f3fc29f63aaefce66e27626420
hobojoe1848 <sequeira.julian@gmail.com>
2015-12-02 22:31:22 +1100

    Big changes to code
    MAJOR CHANGES:
    
    - Changed all pay rate variables to have the word "rate" appended to
    them to further clarify they're the pay rate rather than the time. ie,
    'timeandhalf' variable is now 'timeandhalfrate'.
    
    - Added in a doubletimeandhalfrate variable. (long name yes, but has to
    make sense!).
    
    - Added in a new custom function to the main menu option 1 called
    TimeandHalfEntry(). This function handles the entry of time and a half
    hours worked. It collects the hours, calculates how much you earned,
    displays to screen then returns to the menu.
    
    - Added in placeholders for options 2 and 3.
    
    - Added in a 4th menu option to print the current running total of
    overtime pay earned. Currently broken as I still need to define a global
    variable called total. Function is called PrintTotal().
    
    MINOR CHANGES:
    
    - Added in error checking functionality to main menu so if an invalid
    character type is entered it'll present the error then return you to the
    menu. Same goes for entering an int that's outside the menu option
    numbers.
    
    - Changed all " marks to ' marks for consistency in the code.
    
    - Added in multiple new comments.

--- pay_calculator.py
+++ pay_calculator.py
@@ -5,44 +5,112 @@ refresh = False
 if len(sys.argv) > 1: 
   refresh = True                      # if script is run with cli argument (any string) do the questioning again
 
-if os.path.isfile("userattributes.txt") and not refresh:      #Check to see userattributes file exists (== True not needed) - if refresh chosen go to else
-    print("Welcome Back!")                                    #If it exists, print Welcome Back Message        
+if os.path.isfile('userattributes.txt') and not refresh:      #Check to see userattributes file exists (== True not needed) - if refresh chosen go to else
+    print('Welcome Back!')                                    #If it exists, print Welcome Back Message
+    
 else:
 
     # start creating the new userattributes file or reset values if refresh is chosen
     
-    configfile = open("userattributes.txt", "w")              #If userattributes doesn't exist, create new userattributes file. Open it in variable configfile
+    configfile = open('userattributes.txt', 'w')              #If userattributes doesn't exist, create new userattributes file. Open it in variable configfile
 
     while True:
       try:                                                    # try is an error handling function. Try doing this, if an error occurs, go to the exception.
-        normaltime = float(input('Please enter normal hourly rate: '))  #enter normal hourly pay rate
+        normaltimerate = float(input('Please enter normal hourly rate: '))  #enter normal hourly pay rate
         break                                                 #break our of while loop
       except ValueError:
-        print("Not a number, please try again")         #if anything other than an int is entered, error out.
+        print('Not a number, please try again')         #if anything other than an int is entered, error out.
 
-    #normaltime = float(normaltime)                      #Convert user input to float
+        
+    timeandhalfrate = normaltimerate * 1.5                      #Calculate time and a half and double time rates based on normal pay rate
+    doubletimerate = normaltimerate * 2
+    doubletimeandhalfrate = normaltimerate * 2.5
+
+    timeandhalfrate = round(timeandhalfrate, 4)                 #Round rates to 4 decimal places
+    doubletimerate = round(doubletimerate, 4)
+    doubletimeandhalfrate = round(doubletimeandhalfrate, 4)
     
-    timeandhalf = normaltime * 1.5                      #Calculate time and a half and double time rates based on normal pay rate
-    doubletime = normaltime * 2
+    print('Time and Half Rate: ' + str(timeandhalfrate))    #Display over time rates
+    print('Double Time Rate: ' + str(doubletimerate))
+    print('Double Time and Half Rate: ' + str(doubletimeandhalfrate))
 
-    timeandhalf = round(timeandhalf, 4)                 #Round rates to 4 decimal places
-    doubletime = round(doubletime, 4)
-    
-    print("Time and Half Rate: " + str(timeandhalf))    #Display over time rates
-    print("Double Time Rate: " + str(doubletime))
-
-    configfile.write(str(normaltime) + "\n");           #Write normal, time and half and double time amounts to userattributes file
-    configfile.write(str(timeandhalf) + "\n");
-    configfile.write(str(doubletime) + "\n");
+    configfile.write(str(normaltimerate) + '\n');           #Write normal, time and half and double time amounts to userattributes file
+    configfile.write(str(timeandhalfrate) + '\n');
+    configfile.write(str(doubletimerate) + '\n');
+    configfile.write(str(doubletimeandhalfrate) + '\n');
     
     configfile.close()                                  #close off the userattributes file (configfile variable).
 
 
+#Let's define the functions that will be used by the main menu.
+
+
+def TimeandHalfEntry():
+  print('\n')
+ 
+  while True:
+    try:
+      timeandhalfhours = float(input('How many hours of time and a half did you work? '))
+      break
+    except ValueError:
+      print('Invalid entry. Please enter a number. \n')
+
+  totaltimeandhalf = timeandhalfhours * timeandhalfrate
+
+  print(str(totaltimeandhalf) + '\n')
+  
+
+
+###########################
+
+
+
+def PrintTotal():
+  print('\n')
+  
+  print(str(total) + '\n')
+
+  input('Press any key to return to main menu...')
+  
+
+
 #Now that the userattributes have been collected and stored, let's start the actual program
 
-configfile = open("userattributes.txt", "r")            #open the userattributes file in variable "configfile".
+configfile = open('userattributes.txt', 'r')            #open the userattributes file in variable "configfile".
+
+normaltimerate = float(configfile.readline())           #read line 1 from the configfile and assign that line as a float to normaltimerate variable
+timeandhalfrate = float(configfile.readline())          #read line 2 and assign
+doubletimerate = float(configfile.readline())           #read line 3 and assign
+doubletimeandhalfrate = float(configfile.readline())    #read line 4 and assign
 
 
+while True:
+  print('What rate of Over Time would you like to enter? (Enter the number)')
+  print('(1) Time and a Half (' + str(timeandhalfrate) + ').')
+  print('(2) Double Time (' + str(doubletimerate) + ').')
+  print('(3) Double Time and a Half (' + str(doubletimeandhalfrate) + ').')
+  print('(4) View Current Total Earned.')
+  print('\n')
+
+  try:
+    OTchoice = int(input('Enter Choice: '))
+  except ValueError:
+    print('Invalid character. Please enter a number. \n')
+    continue
+
+  if OTchoice == 1:
+    TimeandHalfEntry()
+  elif OTchoice == 2:
+    print('Placeholder for 2')
+  elif OTchoice == 3:
+    print('Placeholder for 3')
+  elif OTchoice == 4:
+    PrintTotal()
+  else:
+    print('Invalid number. Please try again. \n')
+  
+    
+      
 
 configfile.close()
 
